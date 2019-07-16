627b80d2f9654efa715f8d5ed924426a085598f7
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-07-16 21:48:58 +0530

    Initial commit

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("2+4 =", 2+4)
+	fmt.Println("9.0/4.0 =", 9.0/4.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
