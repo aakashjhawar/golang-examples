392141db256f3156e5f0f78fe70e10a344e3a353
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-07-25 19:41:49 +0530

    Updated Channels

--- channels.go
+++ channels.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	message := make(chan string)
+	message := make(chan string) // Create a new channel with make(chan val-type). Channels are typed by the values they convey
 
-	go func() { message <- "bing" }()
+	go func() { message <- "bing" }() // Send a value into a channel using the channel <- syntax.
+	// Here we send "ping" to the messages channel we made above, from a new goroutine
 
-	msg := <-message
+	msg := <-message // The <-channel syntax receives a value from the channel. Here we’ll receive the "ping" message we sent above and print it out.
 	fmt.Println(msg)
 }
