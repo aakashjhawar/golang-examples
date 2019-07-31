0965e5920053f80fffc3de51cbef45e34cb2b504
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-07-31 11:27:01 +0530

    Non-Blocking channel operation

--- non_blocking_channel_operation.go
+++ non_blocking_channel_operation.go
@@ -19,6 +19,7 @@ func main() {
 	}
 
 	// A non-blocking send works similarly. Here msg cannot be sent to the messages channel, because the channel has no buffer and there is no receiver. Therefore the default case is selected.
+
 	msg := "hi"
 	select {
 	case messages <- msg:
@@ -26,6 +27,7 @@ func main() {
 	default:
 		fmt.Println("no message sent")
 	}
+
 	// We can use multiple cases above the default clause to implement a multi-way non-blocking select. Here we attempt non-blocking receives on both messages and signals.
 
 	select {
